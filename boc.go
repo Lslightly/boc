5762e62987971c019eb1bb334ae452fc4bb67411
lqw@wsl <lqw332664203@mail.ustc.edu.cn>
2025-03-29 15:46:12 +0800

    update package doc

--- boc.go
+++ boc.go
@@ -1,6 +1,5 @@
 // Package boc is a Go implementation of [paper] "When Concurrency Matters: Behaviour-Oriented Concurrency" in OOPSLA2023.
 //
-// See [Documentation] for more details.
 //   - The core data structure is [CownPtr], which is a pointer to a cown.
 //   - The core primitive is [When]. Use [When1], [When2], [When3], [When4], [WhenVec] if possible to leverage generic and type checking.
 //   - Use [EnableTypeCheck] or set "TYPE_CHECK=1" as environment variable can also enable type checking.
@@ -10,9 +9,6 @@
 // This package is rewritten from [boc.rs].
 // The benchmark is provided by [kaist-cp/cs431] and rewritten in Go.
 //
-// Thanks for authors of [kaist-cp/cs431] and [paper].
-//
-// [Documentation]: https://pkg.go.dev/github.com/Lslightly/boc
 // [boc.rs]: https://github.com/Lslightly-courses/cs431/blob/main/homework/src/boc.rs
 // [paper]: https://dl.acm.org/doi/10.1145/3622852
 // [kaist-cp/cs431]: https://github.com/kaist-cp/cs431
